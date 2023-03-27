64d372858d629e2baab707f74879ea346da43dd4
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-27 19:15:57 +0530

    create RegistrationTokenRepository.

---
+++ src/main/java/com/nimbusventure/band/RegistrationToken/RegistrationTokenRepository.java
@@ -0,0 +1,23 @@
+package com.nimbusventure.band.RegistrationToken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface RegistrationTokenRepository extends JpaRepository<RegistrationToken, Long> {
+    Optional<RegistrationToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE RegistrationToken rt " +
+            "SET rt.verifiedAt = ?2 " +
+            "WHERE rt.token = ?1")
+    void setVerifiedAt(String token, LocalDateTime confirmedAt);
+}
