111f4164949bfc80e7a3aebc9f89904c05f77069
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-27 19:16:57 +0530

    rename the TokenType to RegistrationTokenType.

---
+++ src/main/java/com/nimbusventure/band/RegistrationToken/RegistrationTokenType.java
@@ -0,0 +1,6 @@
+package com.nimbusventure.band.RegistrationToken;
+
+public enum RegistrationTokenType {
+    EMAIL_CONFIRM,
+    PASSWORD_RESET
+}
--- src/main/java/com/nimbusventure/band/token/TokenType.java
+++
@@ -1,6 +0,0 @@
-package com.nimbusventure.band.token;
-
-public enum TokenType {
-    EMAIL_CONFIRM,
-    PASSWORD_RESET
-}
