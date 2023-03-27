750acd79991dac98b9a00fcdb64e1c7a070cf29b
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-27 19:07:05 +0530

    update the routes.

--- src/main/java/com/nimbusventure/band/auth/AuthenticationController.java
+++ src/main/java/com/nimbusventure/band/auth/AuthenticationController.java
@@ -16,53 +16,53 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
-    @PostMapping("/checkEmail")
-    public ResponseEntity<Boolean> checkEmail(
+    @PostMapping("/isUserExists")
+    public ResponseEntity<Boolean> isUserExists(
             @Email @RequestParam(name="email") String email
     ) {
-        return ResponseEntity.ok(service.checkEmail(email));
+        return ResponseEntity.ok(service.isUserExists(email));
     }
 
-    @PostMapping("/checkBandId")
-    public ResponseEntity<Boolean> checkBandId(
+    @PostMapping("/isBandExists")
+    public ResponseEntity<Boolean> isBandExists(
             @NotBlank @RequestParam(name="bandId") String bandId
     ) {
-        return ResponseEntity.ok(service.checkBandId(bandId));
+        return ResponseEntity.ok(service.isBandExists(bandId));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<Boolean> register(
             @Valid @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<AuthenticationResponse> authenticate(
             @Valid @RequestBody AuthenticationRequest request
     ) {
         System.out.println(request);
         return ResponseEntity.ok(service.authenticate(request));
     }
 
-    @PostMapping("/forgotPassword")
-    public ResponseEntity<AuthenticationResponse> forgotPassword(
+    @GetMapping("/confirmRegistration")
+    public ResponseEntity<Boolean> confirmRegistration(
+            @RequestParam(name="token") String token
+    ) {
+        return ResponseEntity.ok(service.confirmRegistration(token));
+    }
+
+    @PostMapping("/resetPassword")
+    public ResponseEntity<Boolean> resetPassword(
             @Valid @RequestBody ForgotPasswordRequest request
     ) {
-        return ResponseEntity.ok(service.forgotPassword(request));
+        return ResponseEntity.ok(service.resetPassword(request));
     }
 
-    @GetMapping("/confirmEmail")
-    public ResponseEntity<AuthenticationResponse> confirmEmail(
+    @GetMapping("/confirmResetPassword")
+    public ResponseEntity<Boolean> confirmResetPassword(
             @RequestParam(name="token") String token
     ) {
-        return ResponseEntity.ok(service.confirmEmail(token));
-    }
-
-    @GetMapping("/confirmPasswordReset")
-    public ResponseEntity<AuthenticationResponse> confirmPasswordReset(
-            @RequestParam(name="token") String token
-    ) {
-        return ResponseEntity.ok(service.confirmPasswordReset(token));
+        return ResponseEntity.ok(service.confirmResetPassword(token));
     }
 }
